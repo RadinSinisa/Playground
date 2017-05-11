20b4be8a77adce5f2bfb6818dc45fd79f26497b0
Sinisa Radin <Sinisa Radin>
2017-05-12 00:51:41 +0200

    Add project files.

---
+++ Playground/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Playground
+{
+    class Pair
+    {
+        public int First { get; set; }
+        public int Second { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
